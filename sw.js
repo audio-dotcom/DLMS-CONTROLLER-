c59cd02124c78e7cd31470a451f0aaca9e171199
audio-dotcom <arsokcity00@gmail.com>
2026-08-12 12:18:20 +0700

    Add service worker for caching files

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'dlms-xta-v1';
+const FILES_TO_CACHE = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
